f4ffc088435e2f9dcd89bee1d8d6c18d966ee7a2
loooha <ryuje1029@gmail.com>
2022-10-06 15:18:45 +0900

    20221006

---
+++ src/java001_basic/Java008_operators.java
@@ -0,0 +1,15 @@
+package java001_basic;
+
+public class Java008_operators {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 4;
+		
+		System.out.println(a+b); //7 //산술연산자
+		System.out.println(a+"는(은) 삼입니다."); //3는(은) 삼입니다. //문자열 연결
+		System.out.println("a="+a); //a=3  //문자열 연결
+		System.out.println("결과="+(a+b)); //결과=7 //괄호 주의
+	}
+
+}
---
+++ src/java001_basic/prob/Prob01.java
@@ -0,0 +1,24 @@
+package java001_basic.prob;
+
+/*
+ * num변수의 값이 10의 배수이면 1을 아니면 0을 
+ * res변수에 리턴하는 프로그램을 구현하시오.
+ * [실행결과]
+ * res=0
+ */
+public class Prob01 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		int res = 0;
+		
+//		res = num/10;
+//		
+//		boolean x = (1 == res--) && (0 == res++);
+//		
+//		System.out.println(();
+				
+		
+	}
+
+}
