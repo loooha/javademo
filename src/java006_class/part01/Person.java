5d81cdd1f811fe3b2a1f1ae2b7b205a80be73576
loooha <ryuje1029@gmail.com>
2022-10-12 15:51:49 +0900

    20221012

---
+++ src/java006_class/part01/Person.java
@@ -0,0 +1,15 @@
+package java006_class.part01;
+
+public class Person {
+	String name;
+	int age;
+	char gender;
+
+	public void develop() {
+		System.out.println("개발한다");
+	}
+
+	public void run() {
+		System.out.println("달린다");
+	}
+}
---
+++ src/java006_class/part09/Java073_this.java
@@ -0,0 +1,10 @@
+package java006_class.part09;
+
+public class Java073_this {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
