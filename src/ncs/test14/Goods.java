f16ce28e7f059966084646711e10712ff39315f8
loooha <ryuje1029@gmail.com>
2022-10-26 04:11:06 +0900

    20221026

--- src/ncs/test14/Goods.java
+++ src/ncs/test14/Goods.java
@@ -1,5 +1,47 @@
 package ncs.test14;
 
 public class Goods {
+	private String name;
+	private int price;
+	private int quantity;
+
+	public Goods() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Goods(String name, int price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("");
+	}
 
 }
--- src/ncs/test15/BookListTest.java
+++ src/ncs/test15/BookListTest.java
@@ -1,9 +1,14 @@
 package ncs.test15;
 
+import ncs.test05.Book;
+
 public class BookListTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Book[] book = new Book[3];
+		book[0] = new Book("자바의 정석", "남궁성", 30000, "도우출판", 0.15);
+		book[1] = new Book("열혈강의 자바", "구정은", 29000, "프리렉", 0.2);
+		book[2] = new Book("객체지향 JAVA8", "금영욱", 30000, "북스홈", 0.1);
 
 	}
 
